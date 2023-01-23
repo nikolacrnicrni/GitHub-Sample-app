4496425a48d7d556f9c482b603c9be224c2d73bf
nikolacrnicrni <crnogorac.nikola93@gmail.com>
2023-01-23 09:17:33 +0100

    Room setup

---
+++ app/src/main/java/com/example/githubapp/data/local/GitHubDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.githubapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [],
+    version = 1
+)
+abstract class GitHubDatabase : RoomDatabase()
