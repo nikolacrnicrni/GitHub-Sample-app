b5d3865c630e384a170af8a5e21c8107be7449f5
nikolacrnicrni <crnogorac.nikola93@gmail.com>
2023-01-23 09:41:20 +0100

    Folder structure setup

---
+++ app/src/main/java/com/example/githubapp/data/remote/RemoteRepoImpl.kt
@@ -0,0 +1,7 @@
+package com.example.githubapp.data.remote
+
+import com.example.githubapp.data.repositories.RemoteRepo
+
+class RemoteRepoImpl: RemoteRepo {
+
+}
---
+++ app/src/main/java/com/example/githubapp/data/repositories/RemoteRepo.kt
@@ -0,0 +1,5 @@
+package com.example.githubapp.data.repositories
+
+interface RemoteRepo {
+
+}
